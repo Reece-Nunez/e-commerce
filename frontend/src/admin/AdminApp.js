203c6427bf2d8fe2e80502752eeacd2c2688d445
Reece Nunez <116838102+Reece-Nunez@users.noreply.github.com>
2025-01-26 09:49:24 +0000

    updated prisma schema with correct indexing. added AdminApp.js basic structure

---
+++ frontend/src/admin/AdminApp.js
@@ -0,0 +1,14 @@
+// src/admin/AdminApp.js
+import React from 'react';
+import { Admin, Resource } from 'react-admin';
+import dataProvider from './dataProvider'; // custom or a library
+
+const AdminApp = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="products" />
+    <Resource name="orders" />
+    {/* ... */}
+  </Admin>
+);
+
+export default AdminApp;
