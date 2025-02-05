8ac520dbf11501a092f3cbe7aa041449166a7391
Reece Nunez <116838102+Reece-Nunez@users.noreply.github.com>
2025-02-05 06:38:06 +0000

    created a sales chart component.

---
+++ frontend/src/components/SalesChart.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+function SalesChart({ data }) {
+    return (
+        <ResponsiveContainer width="100%" height={400}>
+            <LineChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="createdAt" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="_sum.totalPrice" stroke="#8884d8" activeDot={{ r: 8 }} />
+            </LineChart>
+        </ResponsiveContainer>
+    );
+}
+
+export default SalesChart;
